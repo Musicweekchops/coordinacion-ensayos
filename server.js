0d5ff659ec5736036430b7676ef39bc8384c1733
Musicweekchops <136385941+Musicweekchops@users.noreply.github.com>
2024-08-20 19:01:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -33,6 +33,15 @@ app.post('/submit', (req, res) => {
         <!DOCTYPE html>
         <html lang="es">
         <head>
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KP9218VH40"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KP9218VH40');
+</script>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Resultados de Coincidencias</title>
